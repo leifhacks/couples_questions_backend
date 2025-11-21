07b468011a479e9f9316408d145c904e9b0ad5d8
Leif <leif.both@leifhacks-apps.com>
2025-11-21 21:50:09 +0100

    before_action :assign_current_initiator_device for answers

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -10,6 +10,7 @@ module Api
       end
 
       before_action :authenticate_user!
+      before_action :assign_current_initiator_device
       before_action -> { validate_with_validator(Validate::Answers::Update) }, only: [:update]
 
       # POST /api/v1/answers
@@ -31,6 +32,13 @@ module Api
         partner_answer = assignment.answers.find { |a| a.user_id != current_user.id }
 
         render json: { my_answer: answer.payload, partner_answer: partner_answer&.payload }
+      end
+
+      def assign_current_initiator_device
+        return unless defined?(Current)
+
+        header_value = request.headers['X-Initiator-Device-Token']
+        Current.initiator_device_token = header_value.presence
       end
     end
   end
