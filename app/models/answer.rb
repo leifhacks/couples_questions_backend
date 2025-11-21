624694636f247090ed8e50d7abff4db1973fb74e
Leif <leif.both@leifhacks-apps.com>
2025-11-21 22:15:43 +0100

    fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -30,7 +30,7 @@ class Answer < UuidRecord
       user.client_devices.each do |device|
         next if Current.skip_broadcast_device?(device)
 
-        AnswerBroadcastWorker.perform_async(device.id, user.id, self.id, include_answer_body)
+        AnswerBroadcastWorker.perform_async(device.id, user.id, self.id, include_answer_body || user.id == self.user.id)
       end
     end
   end
