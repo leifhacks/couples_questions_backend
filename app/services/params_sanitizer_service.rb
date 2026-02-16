2717c3a20efc96dda2310150fae3e59b702fc4b6
Leif <leif.both@leifhacks-apps.com>
2026-02-16 22:57:39 +0100

    .

--- app/services/params_sanitizer_service.rb
+++ app/services/params_sanitizer_service.rb
@@ -12,6 +12,7 @@ class ParamsSanitizerService
     msg_params['refresh_token'] = msg_params['refresh_token'].truncate(10) unless msg_params['refresh_token'].nil?
     msg_params['item_data'] = msg_params['item_data'].to_s.truncate(30) unless msg_params['item_data'].nil?
     msg_params['image_data'] = msg_params['image_data'].to_s.truncate(10) unless msg_params['image_data'].nil?
+    msg_params['audio_data'] = msg_params['audio_data'].to_s.truncate(10) unless msg_params['audio_data'].nil?
     msg_params['receipt_data'] = msg_params['receipt_data'].to_s.truncate(30) unless msg_params['receipt_data'].nil?
     
     msg_params
